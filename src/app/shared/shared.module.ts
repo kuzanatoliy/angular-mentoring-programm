b7adf749a8e27e3d00985ab567a99b7c788eb8ea
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-12-06 20:38:11 +0300

    Add a few components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SearchComponent } from './search/search.component';
+import { NewCourseComponent } from './new-course/new-course.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    BreadcrumbsComponent,
+    SearchComponent,
+    NewCourseComponent
   ],
   imports: [
     CommonModule
@@ -16,7 +22,10 @@ import { LogoComponent } from './logo/logo.component';
   exports: [
     FooterComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    BreadcrumbsComponent,
+    SearchComponent,
+    NewCourseComponent
   ]
 })
 export class SharedModule { }
