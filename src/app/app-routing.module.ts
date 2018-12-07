386a9cff6591db9519c9fa87fef3c9f169bbc244
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-12-07 20:54:53 +0300

    Add simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesComponent } from './shared/courses/courses.component';
+
+const routes: Routes = [
+  { path: 'courses',        component: CoursesComponent },
+  { path: '**',   redirectTo: '/courses', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
