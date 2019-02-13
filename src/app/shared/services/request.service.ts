fa0b219a8ed5cf34c4b4006b4fc42b0588614407
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2019-02-13 20:40:37 +0300

    Http: Add request service

---
+++ src/app/shared/services/request.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { IUser } from '../../interfaces/IUser';
+
+interface IRequestData {
+  [key: string]: number | string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestService {
+  private headers: HttpHeaders = new HttpHeaders();
+
+  constructor(
+    private http: HttpClient,
+  ) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.headers = new HttpHeaders({ token });
+    }
+  }
+
+  public auth(url, userName, password) {
+    return this.http.post(url, { userName, password })
+      .toPromise()
+      .then((user: IUser) => {
+        const { token } = user;
+        if (token) {
+          localStorage.setItem('token', token);
+          this.headers = new HttpHeaders({ token });
+        }
+        return user;
+      });
+  }
+
+  public post(url) {
+    return this.http.post(url, {}, { headers: this.headers }).toPromise();
+  }
+
+  public get(url, data: IRequestData = {}) {
+    const paramsStr: string = Object.entries(data)
+      .map((item) => `${item[0]}=${item[1]}`).join('&');
+    const urlStr: string = paramsStr ? `${ url }?${ paramsStr }` : url;
+    return this.http.get(urlStr, { headers: this.headers }).toPromise();
+  }
+
+  public delete(url) {
+    return this.http.delete(url, { headers: this.headers }).toPromise();
+  }
+}
