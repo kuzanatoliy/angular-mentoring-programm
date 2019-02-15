4c99512122d0dbe8971cf5aa3b241ba0cdfdcc39
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2019-02-15 19:51:53 +0300

    Http: Fix auth service for auth service

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,59 +1,115 @@
-import { async } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
+import { TokenService } from '../../shared/services/token.service';
+
+import { LOGIN_URL, LOGOUT_URL, USER_INFO_URL } from '../../constants/urls';
+
 describe('AuthService', () => {
-  // let service: AuthService;
+  let service: AuthService;
+  let http: HttpTestingController;
 
-  // beforeEach(() => service = new AuthService());
+  const userInfo = {
+    firstName: 'Stepa',
+    lastName: 'Pupkin',
+    userName: 'Some.User',
+  };
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+    providers: [ AuthService, TokenService ],
+  }));
 
-  // TODO Uncomment after fixing course-item-page component
-  /* it('login should return empty object', () => {
-    service.login('', '').then(result => expect(result).toEqual({}));
+  beforeEach(() => {
+    service = TestBed.get(AuthService);
+    http = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+    http.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('login should return empty object', () => {
+    service.login('', '').then((result) => expect(result).toEqual({}));
+
+    const req = http.expectOne(LOGIN_URL);
+    expect(req.request.method).toEqual('POST');
+    req.flush({});
   });
 
   it('login should return object with userInfo', () => {
-    service.login('user', 'user_password')
-      .then(result => expect(result).toEqual({
-        userName: 'user'
-      }));
+    service.login(userInfo.userName, 'User.Password')
+      .then((result) => expect(result).toEqual(userInfo));
+
+    const req = http.expectOne(LOGIN_URL);
+    expect(req.request.method).toEqual('POST');
+    req.flush(userInfo);
   });
 
   describe('should check not authorized functionality', () => {
+    it('checkUserInfo should return empty object', () => {
+      service.checkUserInfo()
+        .then((result) => expect(result).toEqual({}));
+
+      const req = http.expectOne(USER_INFO_URL);
+      expect(req.request.method).toEqual('GET');
+      req.flush({});
+    });
+
     it('getUserInfo should return null', () => {
       const result = service.getUserInfo();
-      expect(result).toBeNull();
+      expect(result).toBeUndefined();
     });
 
     it('isAuthorize should return false', () => {
-      service.isAuthorized().then(isAuth => expect(isAuth).toBeFalsy());
+      service.isAuthorized().then((isAuth) => expect(isAuth).toBeFalsy());
+
+      const req = http.expectOne(USER_INFO_URL);
+      expect(req.request.method).toEqual('GET');
+      req.flush({});
     });
 
     it('logout should return empty object', () => {
-      service.logout().then(result => expect(result).toEqual({}));
+      service.logout().then((result) => expect(result).toEqual({}));
+
+      const req = http.expectOne(LOGOUT_URL);
+      expect(req.request.method).toEqual('POST');
+      req.flush({});
     });
   });
 
   describe('should check authorized functionality', () => {
-    beforeEach(async(() => {
-      service.login('user', 'user_password');
-    }));
+    it('checkUserInfo should return userInfo', () => {
+      service.checkUserInfo()
+        .then((result) => expect(result).toEqual(userInfo));
 
-    it('getUserInfo should return null', () => {
-      const result = service.getUserInfo();
-      expect(result).toBe('user');
+      const req = http.expectOne(USER_INFO_URL);
+      expect(req.request.method).toEqual('GET');
+      req.flush(userInfo);
     });
 
-    it('isAuthorize should return false', () => {
-      service.isAuthorized().then(isAuth => expect(isAuth).toBeTruthy());
+    it('isAuthorize should return true', () => {
+      service.isAuthorized()
+        .then((isAuth) => expect(isAuth).toBeTruthy());
+
+      const req = http.expectOne(USER_INFO_URL);
+      expect(req.request.method).toEqual('GET');
+      req.flush(userInfo);
     });
 
     it('logout should return empty object', () => {
-      service.logout().then(result => expect(result).toEqual({}));
+      service.logout().then((result) => expect(result).toEqual({}));
+
+      const req = http.expectOne(LOGOUT_URL);
+      expect(req.request.method).toEqual('POST');
+      req.flush({});
     });
-  }); */
+  });
 });
