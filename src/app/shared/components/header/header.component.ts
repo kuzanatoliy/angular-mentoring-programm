0b613f7115d1cbeedcd9bde7199cd43bff0b731a
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2019-02-25 19:02:55 +0300

    Ngrx: Fix code style

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth.service';
+
 import { IUser } from 'src/app/interfaces/IUser';
 
 @Component({
