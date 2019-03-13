c8f3be4501f916dac98b01e297cd60fd0dee1060
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2019-03-13 15:59:19 +0300

    Ngrx: remove unused functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Observer, Subscription, from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { IUser } from 'src/app/interfaces/IUser';
 
 import { LOGIN_URL, LOGOUT_URL, USER_INFO_URL } from 'src/app/constants/urls';
-
-import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -29,17 +26,4 @@ export class AuthService {
   public checkUserInfo(): Observable<IUser> {
     return this.http.get(USER_INFO_URL);
   }
-
-  /* public isAuthorized(): Promise<boolean> {
-    if (this.userInfo.userName || !this.tokenService.token) {
-      return Promise.resolve(this.authorized);
-    } else {
-      return this.checkUserInfo()
-        .then(() => Promise.resolve(this.authorized))
-        .catch(() => {
-          this.tokenService.token = null;
-          return Promise.resolve(this.authorized);
-        });
-    }
-  } */
 }
